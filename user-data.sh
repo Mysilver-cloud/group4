8279fce74a95665282b20aabb5c9ee9e29e6f242
anh dang <anh.dang@cloudreach.com>
2022-10-18 17:52:10 +0200

    uploading

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install git
+
 
 git clone https://github.com/Mysilver-cloud/group4-2.git
-
-sudo crontab -e * * * * * root /usr/bin/python3 /home/ubuntu/group4-2/increment.py
