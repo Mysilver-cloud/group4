de6c38f412e9957a8d302dda8b9ea9c7cb64f451
anh dang <anh.dang@cloudreach.com>
2022-10-18 15:21:22 +0200

    uploading

--- user-data.sh
+++ user-data.sh
@@ -2,5 +2,19 @@
 
 sudo apt-get-update
 
-(crontab -l ; echo "00 09 * * 1-5 echo hello") | crontab -
+python - << EOF #!/usr/bin/env python 
+
+with open("test.txt", "r") as file:
+    number = file.read()
+    print(f"incoming number: {number}")
+    new_number = int(number) + 1
+with open("test.txt", "w") as file:
+    file.write(str(new_number))
+    print(f"outcoming number: {new_number}")
+
+EOF
+
+echo "0" > test.txt
+
+(crontab -e ; echo "* * * * * /usr/bin/python3 /home/ubuntu/increment.py") | crontab -
 
